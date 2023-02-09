4abc93fe7d793aed156ce26ddfafd8a1e0e35f64
ranjabi <fikriranjabi02@gmail.com>
2023-02-09 16:36:01 +0700

    feat: register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,23 +1,31 @@
 import { Container, VStack, Text, Input, Button, Flex } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Register: React.FC<{}> = () => (
-  <Container maxW="container.xl" h="100vh" centerContent>
-    <VStack
-      h="full"
-      justify="center"
-      maxW="sm"
-    >
-      <Text fontWeight="semibold" pb={6} align="center" fontSize="2xl">Buat akun sebagai mahasiswa</Text>
+  <Container centerContent>
+    <VStack h="full" justify="center" maxW="sm">
+      <Text fontWeight="semibold" pb={6} align="center" fontSize="2xl">
+        Buat akun sebagai mahasiswa
+      </Text>
       <VStack px={12} spacing={6}>
         <Input placeholder="Nama" />
         <Input placeholder="Email" />
         <Input placeholder="Password" />
-        <Button colorScheme="teal" w="full">Daftar</Button>
+        <Button colorScheme="biru" w="full">
+          Daftar
+        </Button>
         <Text fontSize="sm" align="center">
           Dengan mengklik daftar, kamu menyetujui Persyaratan dan Ketentuan
           Penggunaan CariDosen.
         </Text>
-        <Text>Sudah punya akun?<Text as="span" color="biru.2">masuk</Text> </Text>
+        <Text>
+          Sudah punya akun?{" "}
+          <Link href="/login">
+            <Text as="span" color="biru.600" fontWeight="bold">
+              masuk
+            </Text>
+          </Link>{" "}
+        </Text>
       </VStack>
     </VStack>
   </Container>

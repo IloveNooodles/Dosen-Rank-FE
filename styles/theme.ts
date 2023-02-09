fb185a6b0db43ed3d9273c80b57b659cccd63eff
ranjabi <fikriranjabi02@gmail.com>
2023-02-09 16:35:52 +0700

    feat: init theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,14 +1,36 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-  biru: {
-    1: '#2A3950',
-    2: '#3A7B99',
-    3: '#70AFCE',
-    4: '#A5DEF1'
-  }
-}
+const theme = extendTheme({
+  colors: {
+    biru: {
+      50: "#E2F4F6",
+      100: "#B6E4EA",
+      200: "#89D1DD",
+      300: "#64BFD2",
+      400: "#4CB2CC",
+      500: "#39A5C6",
+      600: "#3098BA",
+      700: "#2686A8",
+      800: "#237495",
+      900: "#144457",
+    },
+  },
+  components: {
+    Container: {
+      baseStyle: {
+        maxW: "container.xl",
+        height: "100vh",
+      },
+    },
+    Button: {
+      variants: {
+        solid: {
+          color: "white",
+          backgroundColor: "biru.800",
+        },
+      },
+    },
+  },
+});
 
-const theme = extendTheme({ colors })
-
-export default theme
+export default theme;

ed0b9744751a96378451bcf0cbb1639fd017425d
Fayza <fayzanadia@gmail.com>
2023-02-14 18:16:00 +0700

    feat: add navigation bar component

---
+++ components/NavigationBar/index.tsx
@@ -0,0 +1,27 @@
+import { dm_sans } from "@/fonts";
+import { useRouter } from 'next/router';
+import { Box, Container, Text, Flex, Button, HStack, Spacer } from "@chakra-ui/react"
+
+const NavigationBar: React.FC<{}> = () => {
+    const router = useRouter()
+    return (
+        <Container py="24px" px="40px">
+            <Flex align="center">
+                <Text fontFamily="heading" fontSize="lg" color="biru.900" fontWeight="bold">
+                    CariDosen
+                </Text>
+                <Spacer/>
+                <HStack spacing="16px">
+                    <Button variant="secondary" onClick={() => router.push('/login')}>
+                        Masuk
+                    </Button>
+                    <Button variant="primary" onClick={() => router.push('/register')}>
+                        Daftar
+                    </Button>
+                </HStack>
+            </Flex>
+        </Container>
+    )
+};
+
+export default NavigationBar;

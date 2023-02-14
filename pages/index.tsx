0866b23640edb898fd42316d72d9aea1b913664b
Fayza <fayzanadia@gmail.com>
2023-02-14 22:19:09 +0700

    feat: add landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,37 @@ import NavigationBar from "@/components/NavigationBar";
 import Footer from "@/components/Footer";
 import { inter } from "@/fonts";
 import styles from "@/styles/Home.module.scss";
-import { Box, Container, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Container, Flex, HStack, VStack, Text, Input, InputGroup, InputRightElement, Icon, keyframes } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
+import { Search2Icon } from "@chakra-ui/icons";
+import { motion, isValidMotionProp } from 'framer-motion';
 
 const Index: React.FC<{}> = () => (
   <Container>
     <Flex direction="column">
       <NavigationBar/>
-      <Container>
-        awkaowkao
-      </Container>
+      <Center minH="fit-content" h="calc(100vh - 88px - 110.4px)">
+        <VStack>
+          <HStack>
+            <Text fontSize="48px" fontWeight="bold">
+              Cari ulasan
+            </Text>
+            <Text fontSize="48px" fontWeight="bold" color="biru.800">
+              dosen
+            </Text>
+          </HStack>
+          <InputGroup>
+            <Input
+              placeholder="Cari universitas, mata kuliah, atau dosen..."
+              _placeholder={{ color: 'netral.400', fontWeight: 'light' }}
+              borderColor="netral.300"
+              focusBorderColor="biru.800"
+              borderRadius="24px"/>
+            <InputRightElement children={<Search2Icon color="netral.400" />} />
+          </InputGroup>
+        </VStack>
+      </Center>
       <Footer/>
     </Flex>
   </Container>

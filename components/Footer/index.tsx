ea2565eb16a5c5b4756c3a94aeffb1ce1ab7e016
Fayza <fayzanadia@gmail.com>
2023-02-14 21:33:30 +0700

    feat: add footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,28 @@
+import { dm_sans } from "@/fonts";
+import { useRouter } from 'next/router';
+import { FiGithub, FiGitlab } from 'react-icons/fi';
+import { Box, Container, Text, Flex, Button, HStack, Spacer, Icon, VStack, } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Footer: React.FC<{}> = () => {
+    const router = useRouter()
+    return (
+        <Container py="24px" px="40px" h="auto">
+            <VStack alignItems="start">
+                <HStack gap="24px">
+                    <Link href="https://github.com/IloveNooodles" target="_blank">
+                        <Icon as={ FiGithub } color="biru.800" w="24px" h="24px" />
+                    </Link>
+                    <Link href="https://gitlab.informatika.org/if3250_2023_k01_06" target="_blank">
+                        <Icon as={ FiGitlab } color="biru.800" w="24px" h="24px" />
+                    </Link>
+                </HStack>
+                <Text color="biru.800">
+                    made with love by FGFGF
+                </Text>
+            </VStack>
+        </Container>
+    )
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import NavigationBar from "@/components/NavigationBar";
+import Footer from "@/components/Footer";
 import { inter } from "@/fonts";
 import styles from "@/styles/Home.module.scss";
-import { Box, Container, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 
 const Index: React.FC<{}> = () => (
-  <NavigationBar/>
+  <Container>
+    <Flex direction="column">
+      <NavigationBar/>
+      <Container>
+        awkaowkao
+      </Container>
+      <Footer/>
+    </Flex>
+  </Container>
 );
 
 export default Index;

f4b5c99a9659220a577aeca7fc785aafd37c6fc4
Muhammad Fikri Ranjabi <13520002@std.stei.itb.ac.id>
2023-02-17 08:05:38 +0700

    fix: text input bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ const Index: React.FC<{}> = () => {
   }, [counter])
   return (
     <Container>
+      {/* <Wrapper /> */}
       <Box position="absolute" w="100%" h="100%" overflow="clip">
         <Image src="/graduation-cap-2.png" alt="Decoration" position="absolute" marginLeft="12%" marginTop="9%" w="11%" pointerEvents="none" userSelect="none" />
         <Image src="/graduation-cap-1.png" alt="Decoration" position="absolute" marginLeft="50%" marginTop="15%" w="45%" pointerEvents="none" userSelect="none" />

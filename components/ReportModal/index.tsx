66893516ae843533d5dab9a2543a2978c6188e3c
Fayza <fayzanadia@gmail.com>
2023-03-03 18:11:26 +0700

    feat: report component
    Co-authored-by: Fikri Ranjabi <ranjabi@users.noreply.github.com>

---
+++ components/ReportModal/index.tsx
@@ -0,0 +1,136 @@
+import { Container, VStack, Text, CheckboxGroup, Checkbox, Input, HStack, Button, Textarea, Flex, Spacer } from "@chakra-ui/react";
+import React, { MouseEventHandler } from "react";
+
+const ReportModal: React.FC<{
+    onClose: MouseEventHandler<HTMLButtonElement> | undefined;show: boolean
+}> = (props) => {
+    const [count, setCount] = React.useState(0);
+    const [data, setData] = React.useState({
+        inappropriateContent: false,
+        violenceOrAbuse: false,
+        copyright: false,
+        harshWord: false,
+        other: false,
+        detail: "",
+    })
+    if (!props.show) {
+        return null;
+    }
+    return (
+        <Flex
+            w="full"
+            position="fixed"
+            backgroundColor="rgba(0, 0, 0, 0.5)"
+            justifyContent='center'
+            alignItems='center'
+            top={0}
+            bottom={0} >
+            <VStack
+                backgroundColor={"white"}
+                width={{ base: "70%", md: "50%", lg: "40%" }}
+                borderRadius={14}
+                padding={10}>
+                <Text
+                    fontSize={{ base: '1rem', md: '1.3rem'}}
+                    fontWeight="bold"
+                    color="biru.800">
+                        Laporkan
+                </Text>
+                <VStack
+                    width="full">
+                    <VStack align="left"
+                        width="full">
+                        <Text
+                            fontSize={{ base: '0.85rem', md: '1.1rem'}}
+                            fontWeight="bold">
+                                Ada isu apa?
+                            </Text>
+                        <CheckboxGroup>
+                            <Checkbox onChange={ 
+                                (e) => setData(
+                                    prevState => 
+                                    ({...prevState, 
+                                    inappropriateContent: e.target.checked})
+                                    )}>
+                                <Text fontSize={{ base: '0.8rem', md: '1rem'}}>Konten tidak sesuai</Text>
+                            </Checkbox>
+                            <Checkbox onChange={ 
+                                (e) => setData(
+                                    prevState => 
+                                    ({...prevState, 
+                                    violenceOrAbuse: e.target.checked})
+                                    )}>
+                                <Text fontSize={{ base: '0.8rem', md: '1rem'}}>Konten mengandung kekerasan atau perundungan</Text>
+                            </Checkbox>
+                            <Checkbox onChange={ 
+                                (e) => setData(
+                                    prevState => 
+                                    ({...prevState, 
+                                    copyright: e.target.checked})
+                                    )}>
+                                <Text fontSize={{ base: '0.8rem', md: '1rem'}}>Konten melanggar hak cipta</Text>
+                            </Checkbox>
+                            <Checkbox onChange={ 
+                                (e) => setData(
+                                    prevState => 
+                                    ({...prevState, 
+                                    harshWord: e.target.checked})
+                                    )}>
+                                <Text fontSize={{ base: '0.8rem', md: '1rem'}}>Konten mengandung kata kasar</Text>
+                            </Checkbox>
+                            <Checkbox onChange={ 
+                                (e) => setData(
+                                    prevState => 
+                                    ({...prevState, 
+                                    other: e.target.checked})
+                                    )}>
+                                <Text fontSize={{ base: '0.8rem', md: '1rem'}}>Lainnya</Text>
+                            </Checkbox>
+                        </CheckboxGroup>
+                    </VStack>
+                    <Spacer/>
+                    <VStack
+                        width="full">
+                        <Text
+                            width="full"
+                            align="left"
+                            fontSize={{ base: '0.85rem', md: '1.1rem'}}
+                            fontWeight="bold">
+                                Silakan berikan informasi lebih detail dan hal yang dapat kami perbaiki!
+                            </Text>
+                        <Textarea
+                            id="reportTextArea"
+                            placeholder="Halaman ini memiliki..."
+                            resize="none"
+                            maxLength={300}
+                            fontSize={{ base: '0.75rem', md: '1rem'}}
+                            onChange={e => {
+                                setCount(e.target.value.length)
+                                setData(
+                                    prevState => 
+                                    ({...prevState, 
+                                    detail: e.target.value})
+                                    )}} />
+                        <Text
+                            width="full"
+                            fontSize={{ base: '0.6rem', md: '0.7rem'}}
+                            align="right">
+                            { count }/300
+                        </Text>
+                    </VStack>
+                    <Spacer/>
+                    <HStack width="full">
+                        <Button onClick={props.onClose} variant="secondary" width="full">
+                            Batal
+                        </Button>
+                        <Button variant="angry" width="full">
+                            Laporkan
+                        </Button>
+                    </HStack>
+                </VStack>
+            </VStack>
+        </Flex>
+    )
+};
+
+export default ReportModal;

08907adfd283703c60d2ead646427f735b9d41fb
fikrikhoironn <fikrikf470@gmail.com>
2023-03-05 14:37:18 +0700

    feat: try to integrate with be but not done yet

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -6,56 +6,131 @@ import { loadCourseDetail } from "@/lib/load-course-detail";
 import { SummaryRatingProps } from "@/interfaces";
 import SummaryRating from "@/components/SummaryRating";
 import ReviewCard, { ReviewCardProps } from "@/components/ReviewCard";
+import {apiInstance} from "@/utils/apiInstance";
 
-export async function getStaticPaths() {
-    return {
-        paths: [{ params: { id: "1" } }],
-        fallback: false,
-    };
+export async function getServerSideProps(context: { query: { id: string; }; }) {
+    const { id } = context.query;
+
+    try {
+        // const courseRes = await apiInstance({}).get(`/course/${id}`);
+        // const courseId = courseRes.data.data.id;
+        const courseId = 2;
+
+        const reviewRes = await apiInstance({}).get(`/reviews/course/?id=${courseId}`);
+        // const courseName = courseRes.data.data.name;
+        const courseName = "Bahasa Arab"
+
+        const reviews = reviewRes.data.data.reviews;
+        const summaryRatings = reviewRes.data.data.ratings
+        const summaryAverageRating = reviewRes.data.data.averageRating
+
+        return { props: { title: courseName, reviews, summaryRatings, summaryAverageRating } };
+    } catch (e) {
+        console.error(e)
+        console.log("error getServerSideProps")
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
 }
 
-export async function getStaticProps() {
-    const courseData = loadCourseDetail();
+export interface CourseRating {
+    kesesuaianDenganSKS: number;
+    kompetensi: number;
+    kesulitan: number;
+    ketersediaanSumber: number;
+}
 
-    const { title, summaryRatings } = courseData;
-
-
-    return {
-        props: {
-            title,
-            summaryRatings
-        },
-    };
+export interface CourseReview {
+    id: number;
+    creatorId: number;
+    creatorName: string;
+    professorId: number;
+    professorName: string;
+    courseId: number;
+    courseName: string;
+    institutionName: string;
+    content: string;
+    upvote: number;
+    downvote: number;
+    createdAt: string;
+    updatedAt: string;
+    rating: CourseRating;
+    averageRating: number;
 }
 
 export interface CoursePageProps {
-    title: string;
-    summaryRatings: SummaryRatingProps[],
-    reviews: ReviewCardProps
+    title: string,
+    reviews: CourseReview[],
+    summaryRatings: CourseRating,
+    summaryAverageRating: number
 }
-
 const Courses: React.FC<CoursePageProps> = ({
-        title,
-        summaryRatings,
-    }) => {
-    const router = useRouter();
-    const { id } = router.query;
+                                                       title,
+                                                       reviews,
+                                                       summaryRatings,
+                                                       summaryAverageRating,
+                                                   }) => {
+    const ratings = [
+        {name: 'Kesesuaian Dengan SKS',
+            value: summaryRatings.kesesuaianDenganSKS},
+        {name: 'Kompetensi yang diperoleh',
+            value: summaryRatings.kompetensi},
+        {name: 'Kesulitan',
+            value: summaryRatings.kesulitan},
+        {name: 'Ketersediaan Sumber Belajar',
+            value: summaryRatings.ketersediaanSumber},
+    ]
 
     return (
         <Container>
             <MainCard>
                 <Flex direction="column" padding={{ base: 4, sm: 8 }} w="full">
-                    <Divider />
-                    {/* TO DO: update summary rating below!! */}
-                    {/* <SummaryRating title={title} summaryRatings={summaryRatings} /> */}
-                    {/* placeholder */}
+                    <SummaryRating title={title} overallRating={summaryAverageRating} summaryRatings={ratings} />
                     <Divider/>
-                    <Text my={6}>82828 Ulasan</Text>
-                    <ReviewCard reviewFor={"course"} likeCount={999} dislikeCount={0}/>
-                    <ReviewCard reviewFor={"course"} likeCount={999} dislikeCount={0}/>
-                    <ReviewCard reviewFor={"course"} likeCount={999} dislikeCount={0}/>
-                    <ReviewCard reviewFor={"course"} likeCount={999} dislikeCount={0}/>
-                    <ReviewCard reviewFor={"course"} likeCount={999} dislikeCount={0}/>
+                    <Text my={6}>{reviews.length} Ulasan</Text>
+                    {reviews.map((review) => {
+                        const {
+                            id,
+                            creatorId,
+                            creatorName,
+                            professorId,
+                            professorName,
+                            courseId,
+                            courseName,
+                            institutionName,
+                            content,
+                            upvote,
+                            downvote,
+                            createdAt,
+                            updatedAt,
+                            rating,
+                            averageRating,
+                        } = review;
+                        return (
+                            <ReviewCard
+                                key={reviews.indexOf(review)}
+                                reviewFor={"course"}
+                                idReview={id}
+                                reviewerName={creatorName}
+                                firstFieldName='Kesesuaian Dengan SKS'
+                                secondFieldName='Kompetensi yang diperoleh'
+                                thirdFieldName='Kesulitan'
+                                fourthFieldName='Ketersediaan Sumber Belajar'
+                                firstFieldRating={rating.kesesuaianDenganSKS}
+                                secondFieldRating={rating.kompetensi}
+                                thirdFieldRating={rating.kesulitan}
+                                fourthFieldRating={rating.ketersediaanSumber}
+                                reviewDate={createdAt}
+                                reviewContent={content}
+                                likeCount={upvote}
+                                dislikeCount={downvote}
+                            />
+                        );
+                    })}
                 </Flex>
             </MainCard>
         </Container>

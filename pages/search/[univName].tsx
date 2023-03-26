a35a8d1588b97ba5724ce072070b446972f316f2
fikrikhoironn <fikrikf470@gmail.com>
2023-03-26 18:07:04 +0700

    feat: search di halaman filter udah jalan

--- pages/search/[univName].tsx
+++ pages/search/[univName].tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import {
     Box, Button,
     Container,
-    Flex, HStack,
+    Flex, HStack, Input, InputGroup, InputRightElement,
     Select, Spacer,
     Stack,
     Tab,
@@ -19,16 +19,18 @@ import DosenCard from "@/components/DosenCard";
 import MatkulCard from "@/components/MatkulCard";
 import { apiInstance } from "@/utils/apiInstance";
 import ContentNotFound from "@/components/ContentNotFound";
+import Link from "next/link";
+import {Search2Icon} from "@chakra-ui/icons";
 
-export async function getServerSideProps(context: { query: { name: string } }) {
-    const { name } = context.query;
+export async function getServerSideProps(context: { query: { univName: string, name?: string, page?: number } }) {
+    const { univName, name, page } = context.query;
 
     try {
         const response  = await apiInstance({})
-            .get(`/search/${name}`)
+            .get(name? `/search/${univName}/?name=${name}&page=${page}` : `/search/${univName}`)
             .catch((e) => console.error(e));
         const { courses, professors } = await response?.data.data;
-        return { props: { courses, professors } };
+        return { props: { univName, courses, professors } };
     }
     catch (e) {
         console.log(e);
@@ -44,6 +46,7 @@ export async function getServerSideProps(context: { query: { name: string } }) {
 export interface SearchAndFilterProps {
     courses: CoursesProps[],
     professors: ProfessorProps[],
+    univName: string,
 }
 
 export interface CoursesProps {
@@ -65,7 +68,9 @@ export interface ProfessorProps {
 const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
     courses,
     professors,
+    univName,
                                                          }) => {
+    const [nameSearch, setNameSearch] = useState<string>("");
     return (
         <Container centerContent h="calc(100vh - 5.5rem - 6.9rem)" w="calc(100vw - 10rem)">
             <Flex justifyContent="center" w="full">
@@ -105,7 +110,24 @@ const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
                                     </TabList>
                                 </Flex>
                                 <Box my={8}>
-                                    <SearchBar/>
+                                    <InputGroup w={{ base: "20rem", md: "33rem" }}>
+                                        <Input
+                                            placeholder="Cari mata kuliah, atau dosen..."
+                                            _placeholder={{ color: "netral.400", fontWeight: "light" }}
+                                            borderColor="netral.300"
+                                            fontSize={{ base: "0.7rem", md: "1rem" }}
+                                            focusBorderColor="biru.800"
+                                            borderRadius="1.5rem"
+                                            backgroundColor={"whiteAlpha.700"}
+                                            backgroundBlendMode="overlay"
+                                            onChange={(e) => setNameSearch(e.target.value)}
+                                        />
+                                        <Link href={`/search/${univName}/?name=${nameSearch}&page=1`}>
+                                            <InputRightElement>
+                                                <Search2Icon color="netral.400" />
+                                            </InputRightElement>
+                                        </Link>
+                                    </InputGroup>
                                 </Box>
                                 <Box pt="1.5rem">
                                     <HStack gap="38rem">
@@ -125,20 +147,28 @@ const SearchAndFilter: React.FC<SearchAndFilterProps> = ({
                                         <Card w="57rem" h="35rem">
                                             {/* eslint-disable-next-line react/jsx-no-undef */}
                                             <Wrap w="95%" h="95%" >
-                                                {professors.length > 0 ? professors.map((professor) => (
+                                                {(professors !== null) ? professors.map((professor) => (
                                                     // eslint-disable-next-line react/jsx-key
                                                     <DosenCard dosenName={professor.name} urlDosen={professor.slug}></DosenCard>
-                                                )) : <ContentNotFound/>}
+                                                )) :
+                                                    <Box w="100%" h="100%">
+                                                        <ContentNotFound/>
+                                                    </Box>}
                                             </Wrap>
                                         </Card>
                                     </TabPanel>
                                     <TabPanel h="35rem">
                                         <Card w="57rem" h="35rem">
                                             <Wrap w="95%" h="95%" >
-                                                {courses.length > 0 ? courses.map((course) => (
+                                                {courses !== null ? courses.map((course) => (
                                                     <MatkulCard
                                                         key={courses.indexOf(course)} matkulName={course.name} matkulCode={course.course_id} urlMatkul={course.course_id}></MatkulCard>
-                                                )) : <ContentNotFound/>}
+                                                )) :
+                                                    <Box w="100%" h="100%">
+                                                        <ContentNotFound/>
+                                                    </Box>
+
+                                                }
                                             </Wrap>
                                         </Card>
                                     </TabPanel>

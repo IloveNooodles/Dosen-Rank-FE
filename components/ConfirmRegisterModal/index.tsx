fb3342de6fffaecb8586a4390c11339b8f15c1ff
fikrikhoironn <fikrikf470@gmail.com>
2023-03-31 10:28:33 +0700

    feat: mobile responsive untuk modal

--- components/ConfirmRegisterModal/index.tsx
+++ components/ConfirmRegisterModal/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Button, Container, Modal, ModalContent, ModalOverlay, Text, VStack} from "@chakra-ui/react";
+import {Box, Button, Container, Modal, ModalContent, ModalOverlay, Show, Text, VStack} from "@chakra-ui/react";
 import {Card} from "@chakra-ui/card";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,16 +13,21 @@ const ConfirmRegisterModal: React.FC<ConfirmRegisterModalProps> = ({isOpen, onCl
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay/>
             <ModalContent>
-                <Card w={"37rem"} h={"27rem"}>
+                <Card w={{sm:"15rem" , md:"37rem"}} h={{sm:"27rem" , md:"27rem"}}>
                     <VStack px={8}>
                         <Box pt={8}>
-                            <Image src={"/ic-mail-sent.svg"} alt={"decorative image"} width={200} height={200}/>
+                            <Show above={"md"}>
+                                <Image src={"/ic-mail-sent.svg"} alt={"decorative image"} width={200} height={200}/>
+                            </Show>
+                            <Show below={"md"}>
+                                <Image src={"/ic-mail-sent.svg"} alt={"decorative image"} width={100} height={100}/>
+                            </Show>
                         </Box>
-                        <Text fontSize="2xl" fontWeight="bold" color={"biru.800"}>
+                        <Text fontSize={{sm:"xl" , md:"2xl"}} fontWeight="bold" color={"biru.800"}>
                             Terima kasih telah mendaftar!
                         </Text>
                         <Text align={"center"} fontSize={"sm"}>
-                            Kami telah mengirimkan email konfirmasi ke [email] untuk mengaktifkan akun kamu.
+                            Kami telah mengirimkan email konfirmasi untuk mengaktifkan akun kamu.
                         </Text>
                         <Box pt={4} pb={4}>
                             <Link href={"/"}>

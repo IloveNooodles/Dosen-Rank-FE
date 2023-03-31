a6d0adbcfac6a9e35d12cfdb53176de5cbdba545
fikrikhoironn <fikrikf470@gmail.com>
2023-03-31 07:31:01 +0700

    reset password 2

--- components/SetNewPasswordForm/index.tsx
+++ components/SetNewPasswordForm/index.tsx
@@ -15,36 +15,38 @@ const SetNewPasswordForm: React.FC = () => {
     return (
         <Box>
             <VStack>
-                <Text fontWeight="semibold"  align="center" fontSize="2xl">
-        Lupa Password?
-        </Text>
-        <Text fontSize={"sm"} color="biru.900">
-        Jangan khawatir, kami akan membantu kamu mereset password.
-    </Text>
-    {/* eslint-disable-next-line react/jsx-no-undef */}
-    <FormControl>
-        <FormLabel>Email address</FormLabel>
-    <Input type='email' />
-    <Button
-        mt={4}
-    colorScheme='teal'
-    type='submit'
-    width="24rem"
-        >
-        Submit
-        </Button>
-        </FormControl>
-        <Text fontSize={"xs"} fontWeight={"400"}>
+                <Text fontWeight="semibold" align="center" fontSize="2xl">
+                    Setting Password Baru
+                </Text>
+                <Text fontSize={"sm"} color="biru.900">
+                    Password baru kamu harus berbeda dengan password kamu sebelumnya.
+                </Text>
+                {/* eslint-disable-next-line react/jsx-no-undef */}
+                <FormControl>
+                    <FormLabel>Password</FormLabel>
+                    <Input type='password'/>
+                    <FormLabel>Konfirmasi Password</FormLabel>
+                    <Input type='password'/>
+                    <Button
+                        mt={4}
+                        colorScheme='teal'
+                        type='submit'
+                        width="24rem"
+                    >
+                        Submit
+                    </Button>
+                </FormControl>
+                <Text fontSize={"xs"} fontWeight={"400"}>
                     ⬅︎ Kembali ke {" "}
-    <Link href="/login">
-    <Text as="span" color="biru.600" fontWeight="bold">
-        login
-        </Text>
-        </Link>
-        </Text>
-        </VStack>
+                    <Link href="/login">
+                        <Text as="span" color="biru.600" fontWeight="bold">
+                            login
+                        </Text>
+                    </Link>
+                </Text>
+            </VStack>
         </Box>
-);
+    );
 };
 
 export default SetNewPasswordForm;
--- pages/reset-password/[token].tsx
+++ pages/reset-password/[token].tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Box, Center, Container, Text, VStack} from "@chakra-ui/react";
 import EnterEmailForm from "@/components/EnterEmailForm";
 import CheckYourEmail from "@/components/CheckYourEmail";
+import SetNewPasswordForm from "@/components/SetNewPasswordForm";
+import PasswordResetted from "@/components/PasswordResetted";
 
 const resetPassword: React.FC<{}> = () => {
     return (
         <Container centerContent h="calc(100vh - 5.5rem - 6.9rem)" w="calc(100vw - 10rem)">
             <Box pt={"2rem"}>
                 <VStack position="relative" h="full" justify="start" maxW="sm" >
-
+                    <PasswordResetted/>
                 </VStack>
             </Box>
         </Container>

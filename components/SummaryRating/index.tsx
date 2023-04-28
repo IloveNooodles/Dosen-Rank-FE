7870ccc3a8668487a50b29c8fabdc8c857e93483
Muhammad Fikri Ranjabi <13520002@std.stei.itb.ac.id>
2023-04-28 18:39:08 +0700

    delete: lihat matkul/dosen

--- components/SummaryRating/index.tsx
+++ components/SummaryRating/index.tsx
@@ -58,30 +58,6 @@ const SummaryRating: React.FC<{
     </Flex>
     {/* action button */}
     <Flex direction="row">
-      {pagePath.includes("professors") ? (
-        <Link href="/">
-          <Text
-            as="u"
-            fontSize={{ base: "sm", sm: "lg" }}
-            mr="4"
-            ml={{ sm: 2, lg: 4 }}
-          >
-            Lihat mata kuliah
-          </Text>
-        </Link>
-      ) : null}
-      {pagePath.includes("courses") ? (
-        <Link href="/">
-          <Text
-            as="u"
-            fontSize={{ base: "sm", sm: "lg" }}
-            mr="4"
-            ml={{ sm: 2, lg: 4 }}
-          >
-            Lihat dosen
-          </Text>
-        </Link>
-      ) : null}
       {pagePath.includes("universities")? (
         <Link href={`/search/${pagePath.split('/')[2]}`}>
           <Text
